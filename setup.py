820f538bfd89516cbbe58d91c5aadd7d84af9452
Andi <andi.gerken@gmail.com>
2025-05-26 15:42:07 +0200

    Added Author Name to setup

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="optuna_tpe_suggestion",
+    author="Lars Andersen Bratholm",
     version="0.1",
-    packages=find_packages(),  # will now find the actual package
+    packages=find_packages(),
     install_requires=["optuna", "numpy"],
     python_requires=">=3.7",
     zip_safe=True,
